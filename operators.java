159b82c04fdb24b9f05c94923be1a1f89ebc95b9
Oluwafisayomi Ogunyemi <oluwafisayomiogunyemi@gmail.com>
2021-10-06 22:01:03 +0100

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,104 @@
+package com.company;
+
+public class operators {
+    public static void main(String[] args) {
+
+                          // Arithmetic Operators//
+       /*
+        int a = 18;
+        int b = 10;
+
+        // Addition
+        int ans = a + b;
+        System.out.println(ans);
+
+        //Subtraction
+        int an = a - b;
+        System.out.println(an);
+
+        //Multiplication
+        int ad = a * b;
+        System.out.println(ad);
+
+        //Division
+        int as = a / b;
+        System.out.println(as);
+
+        //Remainder
+        int ns = a % b;
+        System.out.println(ns);
+
+        // Using + in adding Strings
+        String firstName = "Oluwafisayomi";
+        String lastName = "Ogunyemi";
+
+        String fullname = firstName + " " + lastName;
+        System.out.println(fullname);
+
+        // increasing the value of a variable
+        int ai = 6;
+            ai = ai + 5: // Increase ai by 5
+
+        ai +=2; // increase ai by 2
+
+        ai++; // increase ai with 1
+        ai--; // decrease i by 1
+
+        ai -= 2; // decrease by 2
+
+        ai *= 2; // multiply ai by 2
+        ai /=2; //divide ai by 2
+        System.out.println(ai);
+
+
+        */
+                           // Relational and Logical Operators
+        /*
+                Comparison operators are useful when you want to compare two things ( <, >, ==, >=, <=, != )
+
+             int c = 7;
+             int d = 8;
+
+             boolean answer = a > b; // it would assign the value of true if the syntax is true
+             boolean answer1 = a < b; // it would assign the value of false if the syntax is false
+             boolean ansi = a == b; // it would print true if syntax is true
+             boolean asd = a >= b;  // it would print true if syntax is true
+             boolean ase = a <= b; // it would print true if syntax is true
+             boolean ann = a != b; // check if a is not equal to b
+
+
+
+                              // Logical Operators
+            1. OR
+              boolean new = a==5 || b==2; // if none of the conditions are true assign false
+            2. AND
+                boolean bio = a==5 && b==2; // assign true if both conditions are true ( both conditions must be true)
+
+
+                                // Conditional Statements
+
+             1. IF
+                    if(a > 3) {
+                        System.out.println("A is greater than 3");
+                    } else {
+                            System.out.printLn("A is less than 3")
+                    }
+
+                    System.out.println("Continue ...')
+
+             2. Switch
+                    switch (a) {
+                        case 1:
+                            System.out.ptintln("A is One");
+                            break;
+                        case 2:
+                            System.out.ptintln("A is Two");
+                            break;
+                        default:
+                            System.out.println("A is not One or Two");
+                            break;
+                    }
+
+         */
+    }
+}
